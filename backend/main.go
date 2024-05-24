1f583742870a82a9110ea902b11294305ed77ef1
lzake <lowezake@gmail.com>
2024-05-24 11:01:27 -0400

    adding swagger

--- backend/main.go
+++ backend/main.go
@@ -11,12 +11,30 @@ import (
 	"strconv"
 	"time"
 
+	_ "github.com/lzake/gowebsite-backend/docs"
+
 	"github.com/Masterminds/squirrel"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
+
+// @title Swagger for zach lowe go and angular website
+// @version 1.0
+// @description Swagger for zach lowe go and angular website.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
 
 var statementBuilder = squirrel.StatementBuilder
 
@@ -61,6 +79,14 @@ func readConfig(filename string) (*Config, error) {
 	return &config, nil
 }
 
+// @Summary Get all users
+// @Description Get all users
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} User
+// @Failure 500 {object} map[string]interface{}
+// @Router /users [get]
 func getUsers(db *sql.DB) ([]User, error) {
 	queryBuilder := squirrel.Select("id", "username", "email", "created_at", "updated_at").From("users")
 	sql, args, err := queryBuilder.ToSql()
@@ -85,6 +111,17 @@ func getUsers(db *sql.DB) ([]User, error) {
 	return users, nil
 }
 
+// @Summary Get user by ID
+// @Description Get user by ID
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param id path int true "User ID"
+// @Success 200 {object} User
+// @Failure 400 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /users/{id} [get]
 func getUserByID(db *sql.DB, id int) (User, error) {
 	var user User
 	queryBuilder := squirrel.Select("id", "username", "email", "created_at", "updated_at").From("users").Where(squirrel.Eq{"id": id})
@@ -97,6 +134,16 @@ func getUserByID(db *sql.DB, id int) (User, error) {
 	return user, err
 }
 
+// @Summary Create a new user
+// @Description Create a new user
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param user body User true "User"
+// @Success 201 {object} User
+// @Failure 400 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /users [post]
 func createUser(db *sql.DB, user *User) error {
 	queryBuilder := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar).
 		Insert("users").
@@ -118,6 +165,18 @@ func createUser(db *sql.DB, user *User) error {
 	return nil
 }
 
+// @Summary Update user
+// @Description Update user
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param id path int true "User ID"
+// @Param user body User true "User"
+// @Success 200 {object} User
+// @Failure 400 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /users/{id} [put]
 func updateUser(db *sql.DB, id int, user *User) error {
 	queryBuilder := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar).
 		Update("users").
@@ -198,6 +257,13 @@ func dbConnect(cfg *Config) (*sql.DB, error) {
 	return db, db.Ping()
 }
 
+// @Summary List all users
+// @Description Get a list of all users
+// @Tags users
+// @Produce json
+// @Success 200 {array} User
+// @Failure 500 {object} map[string]interface{}
+// @Router /users [get]
 func main() {
 	config, err := readConfig("config.json")
 	if err != nil {
@@ -223,6 +289,8 @@ func main() {
 
 	e.Validator = &CustomValidator{validator: validator.New()}
 
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
 	e.GET("/users", func(c echo.Context) error {
 		users, err := getUsers(db)
 		if err != nil {
@@ -231,6 +299,16 @@ func main() {
 		return c.JSON(http.StatusOK, users)
 	})
 
+	// @Summary Get a user by ID
+	// @Description Get a user by their ID
+	// @Tags users
+	// @Produce json
+	// @Param id path int true "User ID"
+	// @Success 200 {object} User
+	// @Failure 400 {object} map[string]interface{}
+	// @Failure 404 {object} map[string]interface{}
+	// @Failure 500 {object} map[string]interface{}
+	// @Router /users/{id} [get]
 	e.GET("/users/:id", func(c echo.Context) error {
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
@@ -246,6 +324,16 @@ func main() {
 		return c.JSON(http.StatusOK, user)
 	})
 
+	// @Summary Create a new user
+	// @Description Create a new user with the provided details
+	// @Tags users
+	// @Accept json
+	// @Produce json
+	// @Param user body User true "User"
+	// @Success 201 {object} User
+	// @Failure 400 {object} map[string]interface{}
+	// @Failure 500 {object} map[string]interface{}
+	// @Router /users [post]
 	e.POST("/users", func(c echo.Context) error {
 		var user User
 		if err := c.Bind(&user); err != nil {
@@ -261,6 +349,18 @@ func main() {
 		return c.JSON(http.StatusCreated, user)
 	})
 
+	// @Summary Update an existing user
+	// @Description Update an existing user by their ID
+	// @Tags users
+	// @Accept json
+	// @Produce json
+	// @Param id path int true "User ID"
+	// @Param user body User true "User"
+	// @Success 200 {object} User
+	// @Failure 400 {object} map[string]interface{}
+	// @Failure 404 {object} map[string]interface{}
+	// @Failure 500 {object} map[string]interface{}
+	// @Router /users/{id} [put]
 	e.PUT("/users/:id", func(c echo.Context) error {
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
@@ -283,6 +383,15 @@ func main() {
 		return c.JSON(http.StatusOK, user)
 	})
 
+	// @Summary Delete a user
+	// @Description Delete a user by their ID
+	// @Tags users
+	// @Param id path int true "User ID"
+	// @Success 204 {object} nil
+	// @Failure 400 {object} map[string]interface{}
+	// @Failure 404 {object} map[string]interface{}
+	// @Failure 500 {object} map[string]interface{}
+	// @Router /users/{id} [delete]
 	e.DELETE("/users/:id", func(c echo.Context) error {
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
@@ -297,6 +406,6 @@ func main() {
 		}
 		return c.NoContent(http.StatusNoContent)
 	})
-
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	e.Logger.Fatal(e.Start(":8080"))
 }
